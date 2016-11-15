d08a218c04120a911b258844ac9cc9d7fc582056
Hung Nguyen Dinh <Hung Nguyen Dinh>
2016-11-16 01:17:17 +0700

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,6 +9,38 @@ import { Observable } from 'rxjs/Observable';
 export class AppComponent {
   title = 'Observable';
   constructor() {
+    this.test2();
+  }
+
+  test3() { // doesn't work
+    let increment = document.getElementById('increment');
+    console.log(increment);
+    let decrement = document.getElementById('decrement');
+    console.log(decrement);   
+    let counter = document.getElementById('counter');
+    console.log(counter);    
+    let incrementClick = Observable.fromEvent(increment, 'click');
+    let decrementClick = Observable.fromEvent(decrement, 'click');
+
+    incrementClick.subscribe(event=>console.log(event));
+  }
+
+  test2() {
+    let stream = new Observable(observer => {
+      let counter: number = 0;
+      let interval = setInterval(() => {
+        observer.next(counter++);
+      }, 1000);
+      return () => {
+        clearInterval(interval);
+      }
+    });
+
+    stream.filter((value: number) => value % 2 === 0)
+      .subscribe(value => console.log(value));
+  }
+
+  test1() {
     let promise = new Promise(resolve => {
       setTimeout(() => {
         resolve('promise timeout');
@@ -26,10 +59,9 @@ export class AppComponent {
       }
     });
 
-    let disposible = stream.subscribe(value=>console.log(value));
-    setTimeout(()=>{
+    let disposible = stream.subscribe(value => console.log(value));
+    setTimeout(() => {
       disposible.unsubscribe();
     }, 1000);
-
   }
 }
