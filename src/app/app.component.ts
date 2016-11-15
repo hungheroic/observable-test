1da5f3290638557c102fd23fa21a9621a0ac0bf3
Hung Nguyen Dinh <Hung Nguyen Dinh>
2016-11-15 23:20:47 +0700

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
-
+import { Observable } from 'rxjs/Observable';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Observable';
+  constructor() {
+    let promise = new Promise(resolve => {
+      setTimeout(() => {
+        resolve('promise timeout');
+      }, 2000);
+    });
+
+    promise.then(value => console.log(value));
+
+    let stream = new Observable(observer => {
+      let timeout = setTimeout(() => {
+        observer.next('observable timeout');
+      }, 2000);
+
+      return () => {
+        clearTimeout(timeout);
+      }
+    });
+
+    let disposible = stream.subscribe(value=>console.log(value));
+    setTimeout(()=>{
+      disposible.unsubscribe();
+    }, 1000);
+
+  }
 }
