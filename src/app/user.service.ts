d82b02f6ca29822e3f18a4c9579fd846dc65ea6b
Hung Nguyen Dinh <Hung Nguyen Dinh>
2016-11-19 21:55:51 +0700

    Complete

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,29 +1,39 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 import { User } from '../app/models/user';
 
-const baseUrl:string = 'https://api.github.com/users/hungheroic';
+const baseUrl: string = 'https://api.github.com/users/hungheroic';
 
 @Injectable()
 export class UserService {
-  
+
   private user: User;
-  constructor(private http: Http) { 
+  constructor(private http: Http) {
     //this.user = this.getUser();
   }
-  
-  getUser() {
-    return this.http.get(baseUrl).map((res:Response)=>res.json());
+
+  getUser(): Observable<User> {
+    return Observable.create(observer => {
+      this.http.get(baseUrl)
+        .map((res: Response) => res.json())
+        .subscribe(data => {
+          setTimeout(() => {
+            observer.next(this.mapUser(data));
+            observer.complete();
+          }, 3000);
+        });
+    });
   }
-  get currentUser(){
+  get currentUser() {
     return this.user;
   }
 
-  set currentUser(data){
+  set currentUser(data) {
     this.user = this.mapUser(data);
   }
 
-  private mapUser(data):User {
+  private mapUser(data): User {
     return new User(data.id, data.login);
   }
 
